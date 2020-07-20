01c51bf854c8f1978cf54f5b8bc640b0df485214
kp22 <skunal2222@gmail.com>
2020-07-20 18:11:55 +0530

    Delete googleplus.php

--- application/config/googleplus.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['googleplus']['application_name'] = 'G-serviceOn';
-//$config['googleplus']['client_id'] = '606646900403-8f952vr2ud0qrikvctredf0e6rvn8hd3.apps.googleusercontent.com';
-$config['googleplus']['client_id'] = '681772005647-9v9fv6hb2khg0ibe8hj42ntjq6a7bfi4.apps.googleusercontent.com';
-//$config['googleplus']['client_secret'] = 'EuGWU7mmpshsVCTX-sYsXgS1';
-$config['googleplus']['client_secret'] = 'aXPtQljErPGX-ZBDclfsUsc0';
-$config['googleplus']['redirect_uri'] = 'https://serviceon.co.in/google';
-$config['googleplus']['api_key'] = '';
-?>
